7dc346db7abdafda39fd460a6d37e8554bb818e4
JhinSanchez <81481602+JhinSachez@users.noreply.github.com>
2022-06-10 09:03:48 -0500

    Practica 11 terminada

---
+++ StaticMath/Zap637_07_06_22_BibliotecaEstatica/Encabezado.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace MathLibrary
+{
+	class Arithmetic {
+	public:
+		// returns A + B
+		static double Add(double a, double b);
+
+		//returns A - B 
+		static double Subtract(double a, double b);
+
+		//Returns A * B
+		static double Multiply(double a, double b);
+
+		//Returns A / B
+		static double Divide(double a, double b);
+	};
+}
---
+++ StaticMath/Zap637_07_06_22_BibliotecaEstatica/Source.cpp
@@ -0,0 +1,22 @@
+#include "Encabezado.h"
+
+namespace MathLibrary {
+	double Arithmetic::Add(double a, double b)
+	{
+		return a + b;
+	}
+	double Arithmetic::Subtract(double a, double b)
+	{
+		return a - b;
+	}
+
+	double Arithmetic::Multiply(double a, double b)
+	{
+		return a * b;
+	}
+
+	double Arithmetic::Divide(double a, double b)
+	{
+		return a / b;
+	}
+}
